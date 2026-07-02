2ee7582d0b3f0377a51a33c3a10056ee0bc74124
dvv-45 <dvishnu8425@gmail.com>
2026-07-02 14:48:51 +0530

    Add server setup and package.json for project initialization

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 const app=express();
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes);
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT,()=>console.log("Server is ruuning on port: "+PORT));
