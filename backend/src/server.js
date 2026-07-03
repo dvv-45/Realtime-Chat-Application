a00c28387bdf72f90a90caaab7bfc4ce141d31ef
dvv-45 <dvishnu8425@gmail.com>
2026-07-03 21:57:14 +0530

    Implement user signup functionality with validation and JWT token generation

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,25 @@ import path from "path";
 import dotenv from "dotenv";
 const app=express();
 import authRoutes from "./routes/auth.route.js";
-
+import {connectDB} from "./lib/db.js";
+app.use(express.json());//req.body
 dotenv.config();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+
+
 app.use("/api/auth",authRoutes);
 
-if (ENV.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
-app.listen(PORT,()=>console.log("Server is ruuning on port: "+PORT));
+app.listen(PORT,()=>{
+    console.log("Server is ruuning on port: "+PORT);
+    connectDB();
+});
