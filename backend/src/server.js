7f92bb1b563a789ab895d32bc3c492e441bb9170
dvv-45 <dvishnu8425@gmail.com>
2026-07-04 11:46:42 +0530

    Welcome email implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,20 @@
 import express from "express";
 import path from "path";
-import dotenv from "dotenv";
 const app=express();
 import authRoutes from "./routes/auth.route.js";
 import {connectDB} from "./lib/db.js";
+import {ENV} from "./lib/env.js";
 app.use(express.json());//req.body
 dotenv.config();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 
 
 app.use("/api/auth",authRoutes);
 
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 app.listen(PORT,()=>{
-    console.log("Server is ruuning on port: "+PORT);
+    console.log("Server is running on port: "+PORT);
     connectDB();
 });
