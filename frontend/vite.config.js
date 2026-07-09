4c752978940820c60d256f0c2a9ee82cbc3724f9
dvv-45 <dvishnu8425@gmail.com>
2026-07-09 20:40:15 +0530

    signup implemented on the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+   theme: {
+    extend: {
+      animation: {
+        border: "border 4s linear infinite",
+      },
+      keyframes: {
+        border: {
+          to: { "--border-angle": "360deg" },
+        },
+      },
+    },
+  },
 })
