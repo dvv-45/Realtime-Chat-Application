58ae1050d2853c22d74f51d60c9b9961abc6b29a
dvv-45 <dvishnu8425@gmail.com>
2026-07-22 13:21:21 +0530

    fix: update SameSite attribute for JWT cookie to 'none' for cross-site compatibility

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken=(userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000,//MS
         httpOnly:true,//Prevent XSS attacks:cross-site scripting
-        sameSite:"strict",//Prevent CSRF attacks:cross-site request forgery
+        sameSite:"none",
         secure:ENV.NODE_ENV==="development"?false:true,
     });
     return token;
